23e2ef7c97cc7d61bdf201c633151ad519dced15
Carlos Peña <carlos.07888@gmail.com>
2020-11-07 21:51:22 -0400

    Actualizar contenido

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-var header = document.querySelector(".area-nav");
+const header = document.querySelector(".area-nav");
 const botonMenu = document.querySelector(".barra-toggle");
 const botonSubir = document.querySelector(".boton-subir");
-let menu = document.querySelector(".menu-contenedor");
+const menu = document.querySelector(".menu-contenedor");
+const proyectos = document.querySelector(".grid-proyectos");
+
+// Slider.
+const slider = document.querySelector(".slider");
+const botonCerrarSlider = document.querySelector("#botonCerrarSlider");
+const sliderImagen = document.querySelector(".imagen-seleccionada");
+const sliderLista = document.querySelector("#sliderLista");
 
 eventListeners();
 
@@ -10,6 +17,9 @@ function eventListeners() {
 
 	botonMenu.addEventListener("click", clickBotonMenu);
 	header.addEventListener("click", clickMenu);
+	proyectos.addEventListener("click", abrirSlider);
+	botonCerrarSlider.addEventListener("click", cerrarSlider);
+	sliderLista.addEventListener("click", abrirMiniatura);
 }
 
 function alHacerScroll () {
@@ -28,6 +38,10 @@ function alHacerScroll () {
 	}
 }
 
+function clickBotonMenu () {
+	menu.classList.toggle("hide");
+}
+
 function clickMenu (e) {
 	const target = e.target;
 
@@ -36,6 +50,47 @@ function clickMenu (e) {
 	}
 }
 
-function clickBotonMenu () {
-	menu.classList.toggle("hide");
+function abrirSlider(e) {
+	const target = e.target;
+
+	if (target.hasAttribute("data-imagenes")) {
+		const imagenes = JSON.parse(target.getAttribute("data-imagenes"));
+
+		limpiarSlider();
+
+		for (url of imagenes) {
+			const li = document.createElement("li");
+
+			const img = document.createElement("img");
+			img.src = url;
+			img.className = "imagen-miniatura";
+
+			li.appendChild(img);
+
+			sliderLista.appendChild(li);
+		}
+
+		sliderImagen.src = imagenes[0];
+
+		slider.classList.remove("esconder");
+	}
+}
+
+function cerrarSlider() {
+	slider.classList.add("esconder");
+}
+
+function limpiarSlider() {
+	while (sliderLista.firstChild) {
+		sliderLista.removeChild(sliderLista.firstChild);
+	}
+}
+
+function abrirMiniatura(e) {
+	const target = e.target;
+
+	if (target.classList.contains("imagen-miniatura")) {
+		sliderImagen.src = target.src;
+	}
+	
 }
